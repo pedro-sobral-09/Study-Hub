7a56b87bbfa9aec79a79e66b921bbd1cae6d9374
Pedro <pedrolusobral85@gmail.com>
2026-02-12 17:17:54 -0300

    feat: lab10 de js

---
+++ 01-fundamentals/03-javascript/fcc-js-certification/labs/10-truncate-string-algorithm/index.js
@@ -0,0 +1,14 @@
+function truncateString(str, num){
+  if (str.length > num){
+    const remStr = str.slice(num);
+    // console.log(remStr);
+    const novStr = str.replace(remStr, "...");
+    return novStr;
+  }
+  else {
+    return str;
+  }
+}
+
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
+console.log(truncateString("Absolutely Longer", 2))
