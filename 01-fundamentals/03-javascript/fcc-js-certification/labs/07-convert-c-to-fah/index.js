1d7e2715bf25cf12167f21bc332419354373ec82
Pedro <pedrolusobral85@gmail.com>
2026-02-12 15:58:16 -0300

    feat: Lab07 de js

---
+++ 01-fundamentals/03-javascript/fcc-js-certification/labs/07-convert-c-to-fah/index.js
@@ -0,0 +1,7 @@
+const convertCtoF = argument => argument * (9/5) + 32;
+
+console.log(convertCtoF(0));
+console.log(convertCtoF(-30));
+console.log(convertCtoF(-10));
+console.log(convertCtoF(0));
+console.log(convertCtoF(20));
