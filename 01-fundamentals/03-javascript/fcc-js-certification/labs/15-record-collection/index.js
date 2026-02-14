96cd91c3723ac0aaf53580f5d38a198e53cc27bd
Pedro <pedrolusobral85@gmail.com>
2026-02-14 08:32:37 -0300

    feat: lab15 de js

---
+++ 01-fundamentals/03-javascript/fcc-js-certification/labs/15-record-collection/index.js
@@ -0,0 +1,38 @@
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+function updateRecords(records, id, prop, value){
+  if (value == ``){
+    delete records[id][prop];
+  } 
+  else if (prop != `tracks` && value != ``){
+    records[id][prop] = value;
+  } 
+  else if (prop == `tracks` && value != `` && records[id][prop] == undefined){
+     records[id][prop] = [value];
+  } else if (prop == `tracks` && value != ``){
+     records[id][prop].push(value);
+  }
+  return records;
+}
+
+const newRecords = {...recordCollection};
+updateRecords(newRecords, 2548, `tracks`, `oi`);
+console.log(newRecords);
